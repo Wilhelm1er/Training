1dab98f48f4a01ecff4c935d61f32b255e6ef20b
Wilhelm1er <64429123+Wilhelm1er@users.noreply.github.com>
2020-05-28 11:10:52 +0200

    Add files via upload

---
+++ Training.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Training {
+	private List<String>Exos=new ArrayList<>();
+	private Exercices Exercices;
+
+	public List<String> getExos() {
+		return Exos;
+	}
+
+	public void setExos(List<String> exos) {
+		Exos = exos;
+	}
+}
