e70164adbee7b5454af1c613fb5c5333a9eefba0
Wilhelm <Wilhelm@localhost>
2020-06-10 22:31:45 +0200

    Ajout des exercices de gainage

--- Training/src/Application.java
+++ Training/src/Application.java
@@ -1,29 +1,55 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) throws InterruptedException {
-	
-		Training T=new Training();
-	
-		Boolean Choix = false;
-		do{
-			System.out.println("Selectionner votre session: ");
-			System.out.println("1 - Débutant: ");
-			System.out.println("2 - Intermédiaire: ");
-			System.out.println("3 - Confirmé: ");
-			System.out.println("4 - Elite: ");
-			int choice = new Scanner(System.in).nextInt();
-			T.Training(choice);
-			T.choix();
-			System.out.println("Pouvons nous commencer l'entrainement? o/n");
-			String rep = new Scanner(System.in).nextLine();
-			if (rep.equals("o")) {
-				Choix = true;
-			}
-		}while (Choix == false);
+		System.out.println("BONJOUR");
+		System.out.println("Que souhaitez vous faire: ");
+		
+		System.out.println("1 - Training: ");
+		System.out.println("2 - Gainage: ");
+		int choice_exercice = new Scanner(System.in).nextInt();
+		
+		if(choice_exercice==1) {
+				Training Renfo=new Training();
+				
+				Boolean Choix_2 = false;
+				do{
+					System.out.println("Selectionner votre session: ");
+					System.out.println("1 - Débutant: ");
+					System.out.println("2 - Intermédiaire: ");
+					System.out.println("3 - Confirmé: ");
+					System.out.println("4 - Elite: ");
+					int choice = new Scanner(System.in).nextInt();
+					Renfo.Renforcement(choice);
+					Renfo.choix_Renforcement();
+					System.out.println("Pouvons nous commencer l'entrainement? o/n");
+					String rep = new Scanner(System.in).nextLine();
+					if (rep.equals("o")) {
+						Choix_2 = true;
+					}
+				}while (Choix_2 == false);
+				}
+				
+		if(choice_exercice==2) {
+				Training Gain=new Training();
+				
+				Boolean Choix_3 = false;
+				do{
+					System.out.println("Selectionner votre session: ");
+					System.out.println("1 - Routine 1: ");
+					System.out.println("2 - Routine 2: ");
+					int choice = new Scanner(System.in).nextInt();
+					Gain.Gainage(choice);
+					Gain.choix_Gainage();
+					System.out.println("Pouvons nous commencer l'entrainement? o/n");
+					String rep = new Scanner(System.in).nextLine();
+					if (rep.equals("o")) {
+						Choix_3 = true;
+					}
+				}while (Choix_3 == false);
+				
+		}
 		
 		System.out.println("Mettez vous en place");
 		System.out.print("Démarrage dans: ");
@@ -31,8 +57,7 @@ public class Application {
 			System.out.println(i);
 			Thread.sleep(1000);
 		}
-		System.out.print("Go: ");
-		
-		
+		System.out.println("Go: ");
+
 	}
 }
--- Training/src/Training.java
+++ Training/src/Training.java
@@ -3,46 +3,73 @@ import java.util.Map;
 
 public class Training {
 	
-	private Map<String, Integer> List = new HashMap<String, Integer>();
+	private Map<String, Integer> List_1 = new HashMap<String, Integer>();
+	private Map<String, Integer> List_2 = new HashMap<String, Integer>();
 	
-	public Map<String, Integer> Training(int level) {
+	public Map<String, Integer> Renforcement(int level) {
 		switch (level) {
 		case 1:
-			List.put("Abdo rameur", 15);
-			List.put("Bras tendu coude", 10);
-			List.put("Tractions", 5);
-			List.put("Mountain Climbers", 15);
-			List.put("Pompes", 5);
+			List_1.put("Abdo rameur", 15);
+			List_1.put("Bras tendu coude", 10);
+			List_1.put("Tractions", 5);
+			List_1.put("Mountain Climbers", 15);
+			List_1.put("Pompes", 5);
 			break;
 		case 2:
-			List.put("Abdo rameur", 20);
-			List.put("Bras tendu coude", 12);
-			List.put("Tractions", 7);
-			List.put("Mountain Climbers", 18);
-			List.put("Pompes", 7);
+			List_1.put("Abdo rameur", 20);
+			List_1.put("Bras tendu coude", 12);
+			List_1.put("Tractions", 7);
+			List_1.put("Mountain Climbers", 18);
+			List_1.put("Pompes", 7);
 			break;
 		case 3:
-			List.put("Abdo rameur", 25);
-			List.put("Bras tendu coude", 20);
-			List.put("Tractions", 15);
-			List.put("Mountain Climbers", 25);
-			List.put("Pompes", 15);
+			List_1.put("Abdo rameur", 25);
+			List_1.put("Bras tendu coude", 20);
+			List_1.put("Tractions", 15);
+			List_1.put("Mountain Climbers", 25);
+			List_1.put("Pompes", 15);
 			break;
 		case 4:
-			List.put("Abdo rameur", 30);
-			List.put("Bras tendu coude", 25);
-			List.put("Tractions", 20);
-			List.put("Mountain Climbers", 30);
-			List.put("Pompes", 20);
+			List_1.put("Abdo rameur", 30);
+			List_1.put("Bras tendu coude", 25);
+			List_1.put("Tractions", 20);
+			List_1.put("Mountain Climbers", 30);
+			List_1.put("Pompes", 20);
 			break;
 		}
-		return List;
+		return List_1;
 	}
+	
+	public  Map<String, Integer> Gainage(int level) {
+		switch (level) {
+		case 1:
+			List_2.put("La Planche", 60);
+			List_2.put("Superman", 60);
+			List_2.put("La Planche Latérale Gauche", 45);
+			List_2.put("La Planche Latérale Droite", 45);
+			List_2.put("La Planche Dos", 60);
+			break;
+		case 2:
+			List_2.put("La Planche", 60);
+			List_2.put("La Planche Latérale Gauche avec torsion", 30);
+			List_2.put("La Planche Latérale Droite avec torsion", 30);
+			List_2.put("La Planche 1 bras 1 jambe levée gauche", 30);
+			List_2.put("La Planche 1 bras 1 jambe levée droit", 30);
+			break;
+		}
+		return List_1;	
+	} 		
 
-	public void choix() {
+	public void choix_Renforcement() {
 		System.out.println("Session: ");
-		for (String s : List.keySet()) {
-			System.out.println("Exercices: " + s + " Répétitions: " + List.get(s));
+		for (String s : List_1.keySet()) {
+			System.out.println("Exercices: " + s + " Répétitions: " + List_1.get(s));
+		}
+	}
+	public void choix_Gainage() {
+		System.out.println("Session: ");
+		for (String s : List_2.keySet()) {
+			System.out.println("Exercices: " + s + " Répétitions: " + List_2.get(s));
 		}
 	}
 }
