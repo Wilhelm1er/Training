5c576a2dfaa2fb38c9c3c00ce46f249e024ef0c4
Guillaume <guillaume.c@protonmail.ch>
2020-06-11 09:39:06 +0200

    Ajout Timer pour decharger le main

---
+++ Training/src/Timer.java
@@ -0,0 +1,45 @@
+import java.util.Map;
+
+public class Timer {
+
+	public Timer(Map<String, Integer> List,String type) throws InterruptedException {
+		switch (type) {
+		case "Renforcement":
+			
+			System.out.println("Mettez vous en place");
+			System.out.print("Démarrage dans: ");
+			
+			for(int i=5;i>0;i--) {
+				System.out.println(i);
+				Thread.sleep(1000);
+			}
+			
+			System.out.println("Go: ");
+			
+			for (String mapentry : List.keySet()) {
+			System.out.println("Exercice: "+mapentry);
+			}
+			
+			break;
+			
+		case "Gainage":
+			
+			System.out.println("Mettez vous en place");
+			System.out.print("Démarrage dans: ");
+			
+			for(int i=5;i>0;i--) {
+				System.out.println(i);
+				Thread.sleep(1000);
+			}
+			
+			System.out.println("Go: ");
+			
+			for (String mapentry : List.keySet()) {
+			System.out.println("Exercice: "+mapentry);
+			}
+			
+			break;
+		}
+	}
+	
+	}
