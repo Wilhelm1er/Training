799b8c18099ce045942ffccb581c458126911a00
cheronguillaume <cheronguillaume@localhost>
2020-07-16 17:58:32 +0200

    Debut Swing

--- Training/src/Main.java
+++ Training/src/Main.java
@@ -3,6 +3,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+import Swing.Vue;
+
 /**
  * Application de Training, Version Java de l'appli produite par Ghost en Python
  * 3
@@ -15,25 +17,24 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException{
 		
 		Menu menu=new Menu();
+		Vue vue= new Vue();
+		
+		vue.authent();
 		
 		//Recuperation date du jour
 		DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
 		java.sql.Date dateS = new java.sql.Date(date.getTime());
-		//System.out.println(format.format(date));
 		
 		System.out.println("#####################################");
-		System.out.println("     TRAINING made in Java  v1.0     ");
+		System.out.println("     TRAINING made in Java  v2.0     ");
 		System.out.println("#####################################");
 		System.out.println(" ");
 		menu.menu_user();
-		// Affichage utilisateur enregistre
 		
 		System.out.println(" ");
 		System.out.println("#####################################");
 		System.out.println(" ");
-	
-		//poids.ajout(name, poids, dateS);
 		
 	}
 }
